8e0933c8e553035920ef3ce63ed29195c00d2f91
Eric <Eric.Sureshkumar@trumpf.com>
2025-11-05 16:30:22 +0530

    R02

--- A02/Program.cs
+++ A02/Program.cs
@@ -3,18 +3,32 @@
 // Copyright (c) Metamation India.
 // ------------------------------------------------------------------
 // Program.cs
-// This program lets the user guess a random number between 1 to 100. And shows feedback
+// This program lets the user guess a random number between 1 to 100.
+// This program also provides feedback after each attempt.
 // ------------------------------------------------------------------------------------------------
 using static System.Console;
 
-int randomNum = new Random ().Next (1, 101);
+int numToGuess = new Random ().Next (1, 101), attempt = 1;
 while (true) {
-   Write ("Guess the number between 1 to 100: ");
-   if (int.TryParse (ReadLine (), out int num) && num < 101 && num > 0) {
-      if (randomNum == num) {
-         Write ($"You guessed correctly. The number is {randomNum}.\n");
-         break;
-      } else WriteLine (num > randomNum + 5 ? "Your guess is too high.\n" : num < randomNum - 5 ?
-         "Your guess is too low.\n" : "Your guess is close!\n");
-   }
+   if (attempt != 1) WriteLine ($"Attempt: {attempt}");
+   Write ("Guess the number between 1 to 100 or 'G' to give up: ");
+   var inp = ReadLine ()?.Trim ().ToLower ();
+   if (inp == "g") break;
+   if (int.TryParse (inp, out int guess) && guess < 101 && guess > 0) {
+      attempt++;
+      if (numToGuess == guess) {
+         Write ($"You guessed correctly. The number is {numToGuess}." +
+            $"\n\nPress 'P' to play again or 'E' to exit.");
+         ConsoleKey key;
+         while ((key = ReadKey (true).Key) != ConsoleKey.P && key != ConsoleKey.E)
+            Write ("\n\nInvalid input. Press 'P' to play again or 'E' to exit.");
+         if (key == ConsoleKey.E) break;
+         WriteLine ("\n");
+         attempt = 1;
+         numToGuess = new Random ().Next (1, 101);
+         continue;
+      }
+      WriteLine (guess > numToGuess ?
+         "Your guess is high. Try again.\n" : "Your guess is low. Try again.\n");
+   } else WriteLine ("Invalid input.\n");
 }
