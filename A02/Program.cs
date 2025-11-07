639ad771f4b8a2c4a8ac064ecc240026f773bbc6
Eric <Eric.Sureshkumar@trumpf.com>
2025-11-07 09:37:30 +0530

    R03

--- A02/Program.cs
+++ A02/Program.cs
@@ -8,27 +8,21 @@
 // ------------------------------------------------------------------------------------------------
 using static System.Console;
 
-int numToGuess = new Random ().Next (1, 101), attempt = 1;
 while (true) {
-   if (attempt != 1) WriteLine ($"Attempt: {attempt}");
-   Write ("Guess the number between 1 to 100 or 'G' to give up: ");
-   var inp = ReadLine ()?.Trim ().ToLower ();
-   if (inp == "g") break;
-   if (int.TryParse (inp, out int guess) && guess < 101 && guess > 0) {
-      attempt++;
-      if (numToGuess == guess) {
-         Write ($"You guessed correctly. The number is {numToGuess}." +
-            $"\n\nPress 'P' to play again or 'E' to exit.");
-         ConsoleKey key;
-         while ((key = ReadKey (true).Key) != ConsoleKey.P && key != ConsoleKey.E)
-            Write ("\n\nInvalid input. Press 'P' to play again or 'E' to exit.");
-         if (key == ConsoleKey.E) break;
-         WriteLine ("\n");
-         attempt = 1;
-         numToGuess = new Random ().Next (1, 101);
+   int numToGuess = 80, attempt = 0, guess = 0;
+   while (guess != numToGuess) {
+      Write ("Guess the number between 1 to 100: ");
+      if (!int.TryParse (ReadLine (), out guess) || guess > 100 || guess < 1) {
+         WriteLine ("Invalid input.\n");
          continue;
       }
+      attempt++;
+      if (guess == numToGuess) break;
       WriteLine (guess > numToGuess ?
-         "Your guess is high. Try again.\n" : "Your guess is low. Try again.\n");
-   } else WriteLine ("Invalid input.\n");
+        "Your guess is high. Try again.\n" : "Your guess is low. Try again.\n");
+   }
+   Write ($"You guessed correctly in {attempt} attempt{(attempt > 1 ? "s" : "")}. " +
+      $"The number is {numToGuess}." + $"\n\nPress Enter to play again!");
+   if (ReadKey ().Key != ConsoleKey.Enter) break;
+   Clear ();
 }
