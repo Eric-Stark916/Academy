04669fc109a3f61da7c520f4544c963ce107ed48
Eric <Eric.Sureshkumar@trumpf.com>
2025-11-11 11:46:11 +0530

    R06:MinorFix

--- A02/Program.cs
+++ A02/Program.cs
@@ -8,20 +8,19 @@
 // ------------------------------------------------------------------------------------------------
 using static System.Console;
 
+const int min = 1, max = 101;
 while (true) {
-   int numToGuess = new Random ().Next (1, 101), attempt = 0;
+   int numToGuess = new Random ().Next (min, max);
    while (true) {
       Write ("Guess the number between 1 to 100: ");
-      if (!int.TryParse (ReadLine (), out int guess) || guess is < 1 or > 100) {
+      if (!int.TryParse (ReadLine (), out int guess) || guess is < min or >= max) {
          WriteLine ("Invalid input.\n");
          continue;
       }
-      attempt++;
       if (guess == numToGuess) break;
       WriteLine ($"Your guess is too {(guess > numToGuess ? "high" : "Low")}.\n");
    }
-   Write ($"You guessed correctly in {attempt} attempt{(attempt > 1 ? "s" : "")}. " +
-      $"The number is {numToGuess}." + $"\n\nPress Enter to play again!");
+   Write ($"You guessed correctly. The number is {numToGuess}.\n\nPress Enter to play again!");
    if (ReadKey ().Key != ConsoleKey.Enter) break;
    Clear ();
 }
