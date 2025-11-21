d0ebf71b66596c6898f714e1fa56887ac50fad09
Eric <Eric.Sureshkumar@trumpf.com>
2025-11-21 14:26:56 +0530

    A02.1: Removed Exit Option

--- A02.1/Program.cs
+++ A02.1/Program.cs
@@ -9,23 +9,28 @@
 using static System.Console;
 
 char key; int maxNum = 100, minNum = 1, midNum = 50, count = 0;
-WriteLine ("Welcome to \"Computer Guessing Game\".\n" +
-           "Game Rules:\nS => Accept the guess or to start the game.," +
-           "\nH => Your number is higher than the guessed one., " +
-           "\nL => Your number is lower than the guessed one.," +
-           "\nX => Exit the game.\n\nLets start the game! Guess a number between 0 to 100.");
-while ((key = char.ToLower (ReadKey (intercept: true).KeyChar)) != 's')
-   if (key == 'x') return;
+WriteLine ("""
+   Welcome to "Computer Guessing Game"
+
+   Game Rules:
+   S => Accept the guess or to start the game.
+   H => Your number is higher than the guessed one.
+   L => Your number is lower than the guessed one.
+
+   Let's start the game! Guess a number between 0 to 100.
+   
+   """);
+while ((key = char.ToLower (ReadKey (intercept: true).KeyChar)) != 's');
 while (count < 7) {
    count++;
    Write ($"Is the number {midNum}? ");
    do key = char.ToLower (ReadKey (true).KeyChar);
-   while (key is not ('s' or 'h' or 'l' or 'x'));
+   while (key is not ('h' or 'l' or 's'));
    Write ($"{key}\n");
-   if (key is 'x' or 's') break;
-   else if (key == 'h') minNum = midNum + 1;
+   if (key == 'h') minNum = midNum + 1;
    else if (key == 'l') maxNum = midNum - 1;
+   else if (key == 's') break;
    midNum = (maxNum + minNum) / 2;
 }
-if (count > 6 && key != 's') WriteLine ("\nAh! I could not guess the number.");
-else if (key == 's') WriteLine ($"\nFinally, I found the number, its: {midNum}!");
+if (key == 's') WriteLine ($"\nFinally, I found the number, its: {midNum}!");
+else WriteLine ("\nAh! I could not guess the number.");
