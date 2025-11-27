31bcc7f76a5abaf1bb219f7b54bf8457712e56c0
Eric <Eric.Sureshkumar@trumpf.com>
2025-11-27 11:42:53 +0530

    A02.1: Reduced Code Lines

--- A02.1/Program.cs
+++ A02.1/Program.cs
@@ -4,33 +4,26 @@
 // ------------------------------------------------------------------
 // Program.cs
 // Program on A02.1: Computer Guessing Game.
-// This program finds the number you are thinking, between 0 to 100 within 7 guesses.
+// This program finds the number you are thinking, between 1 to 100 within 7 guesses.
 // ------------------------------------------------------------------------------------------------
 using static System.Console;
 
-char key; int maxNum = 100, minNum = 1, midNum = 50, count = 0;
-WriteLine ("""
-   Welcome to "Computer Guessing Game"
-
-   Game Rules:
-   S => Accept the guess or to start the game.
-   H => Your number is higher than the guessed one.
-   L => Your number is lower than the guessed one.
-
-   Let's start the game! Guess a number between 0 to 100.
-   
-   """);
-while ((key = char.ToLower (ReadKey (intercept: true).KeyChar)) != 's');
-while (count < 7) {
-   count++;
-   Write ($"Is the number {midNum}? ");
-   do key = char.ToLower (ReadKey (true).KeyChar);
-   while (key is not ('h' or 'l' or 's'));
-   Write ($"{key}\n");
-   if (key == 'h') minNum = midNum + 1;
-   else if (key == 'l') maxNum = midNum - 1;
-   else if (key == 's') break;
-   midNum = (maxNum + minNum) / 2;
+while (true) {
+   char key; int maxNum = 101, minNum = 0, midNum = 50; // maxNum=101 to include 100 in guessing.
+   WriteLine ("Welcome to \"Computer Guessing Game\"\nThink of a number from 1 to 100 and press S to start the game.\n");
+   while (char.ToLower (ReadKey (intercept: true).KeyChar) != 's');
+   while (true) {
+      Write ($"My guess is {midNum}. Is your number higher(H), lower(L), or correct(S) ?");
+      do key = char.ToLower (ReadKey (true).KeyChar);
+      while (key is not ('h' or 'l' or 's'));
+      Write ($"{key}\n");
+      if (key == 'h') minNum = midNum;
+      else if (key == 'l') maxNum = midNum;
+      else if (key == 's') break;
+      midNum = (maxNum + minNum) / 2;
+      if (midNum <= minNum) break;
+   }
+   WriteLine ((key == 's' ? $"\nFinally, I found the number! it's: {midNum}." : "\nAh! I could not guess the number.") +
+                                                                                " Press Enter to play again.\n");
+   if (ReadKey ().Key != ConsoleKey.Enter) break;
 }
-if (key == 's') WriteLine ($"\nFinally, I found the number, its: {midNum}!");
-else WriteLine ("\nAh! I could not guess the number.");
