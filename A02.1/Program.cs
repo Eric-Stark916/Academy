8a89de83cb19dcf891f8492fc0987b4c9b2e8a0a
Eric <Eric.Sureshkumar@trumpf.com>
2025-12-02 16:34:47 +0530

    A02.1: Minor Change

--- A02.1/Program.cs
+++ A02.1/Program.cs
@@ -11,18 +11,17 @@ using static System.Console;
 while (true) {
    char key; int maxNum = 100, minNum = 1, guess = 50;
    WriteLine ("Welcome to \"Computer Guessing Game\"\nThink of a number from 1 to 100 and press S to start the game.\n");
-   while (char.ToLower (ReadKey (intercept: true).KeyChar) != 's');
+   while (char.ToLower (ReadKey (true).KeyChar) != 's');
    while (true) {
       Write ($"My guess is {guess}. Is your number higher(H), lower(L), or correct(S) ?");
       do key = char.ToLower (ReadKey (true).KeyChar);
       while (key is not ('h' or 'l' or 's'));
       Write ($"{key}\n");
-      if (key == 'h') minNum = guess + 1;
-      else if (key == 'l') maxNum = guess - 1;
-      else if (key == 's') {
+      if (key == 's') {
          WriteLine ($"\nFinally, I found the number! it's: {guess}. Press Enter to play again.\n");
          break;
-      }
+      } else if (key == 'h') minNum = guess + 1;
+      else if (key == 'l') maxNum = guess - 1;
       if (minNum > maxNum) {
          WriteLine ("\nAh! I could not guess the number. Press Enter to play again.\n");
          break;
