d2126e3ae860d0fb0268bbf091ca0ee1e93a29a1
Eric <Eric.Sureshkumar@trumpf.com>
2025-12-12 11:32:36 +0530

    A07: Improved Efficiency

--- A07/Program.cs
+++ A07/Program.cs
@@ -20,8 +20,8 @@ class Program {
          if (input == "X") break;
          if (Double.TryParse (input, out double result)) {
             WriteLine ($"The converted value: {result}");
-            //WriteLine ($"Adding five to the resultant{result + 5}"); // To check if the string is actually converted into number.
-         } else WriteLine ("Formatting is wrong!");
+            //WriteLine ($"Adding five to the resultant {result + 5}"); // To check if the string is actually converted into number.
+         } else WriteLine ("Format is wrong!");
       }
    }
    #endregion
@@ -30,47 +30,49 @@ class Program {
 
 #region class Double ------------------------------------------------------------------------------
 class Double {
+   #region Implementation -------------------------------------------
+   // Checks whether the input character is a number.
+   static bool IsNum (char c) => c >= '0' && c <= '9';
+   #endregion
+
    #region Method ---------------------------------------------------
    /// <summary>
    /// Converts a string representation of a number to a double equivalent and returns true if parsing succeeds; otherwise, false.
    /// </summary>
-   /// <param name="inp">String input for conversion.</param>
-   /// <param name="result">The double value produced from the conversion, if successful.</param>
-   /// <returns>True if the string was successfully parsed to a double; otherwise, false.</returns>
-   public static bool TryParse (string inp, out double result) {
-      result = double.NaN;
-      if (string.IsNullOrWhiteSpace (inp)) return false;
-      inp = inp.Trim ().ToUpper ();
+   public static bool TryParse (string str, out double result) {
+      result = 0;
+      if (string.IsNullOrWhiteSpace (str)) return false;
+      str = str.Trim ().ToUpper ();
       double numValue = 0; int dotPosition = 0, exponentValue = 0;
       bool isNegative = false, hasExponent = false, hasDot = false;
-      for (int i = 0; i < inp.Length; i++) {
+      for (int i = 0; i < str.Length; i++) {
          // Converts a string character to its equivalent number.
-         if (char.IsDigit (inp[i])) {
-            numValue = numValue * 10 + (inp[i] - '0');
+         if (IsNum (str[i])) {
+            numValue = numValue * 10 + (str[i] - '0');
             if (hasDot) dotPosition++;
             continue;
          }
         // Checks for dot in the string.
-        else if (inp[i] == '.') {
-            if (hasDot || !((i > 0 && char.IsDigit (inp[i - 1])) || (i + 1 < inp.Length && char.IsDigit (inp[i + 1]))))
+        else if (str[i] == '.') {
+            if (hasDot || !(i > 0 && IsNum (str[i - 1]) && i + 1 < str.Length && IsNum (str[i + 1])))
                return false;
             hasDot = true;
             continue;
          }
         // Checks for Exponential notation and value in the string.
-        else if (inp[i] == 'E') {
-            if (hasExponent || i + 1 >= inp.Length || !(i > 0 && (char.IsDigit (inp[i - 1]) || inp[i - 1] == '.')))
+        else if (str[i] == 'E') {
+            if (hasExponent || i + 1 >= str.Length || !(i > 0 && IsNum (str[i - 1])))
                return false;
-            if (!int.TryParse (inp[(i + 1)..], out exponentValue) || exponentValue > 308 || exponentValue < -324)
+            if (!int.TryParse (str[(i + 1)..], out exponentValue) || exponentValue > 308 || exponentValue < -324)
                return false;
             hasExponent = true;
             break;
          }
         // Checks for leading sign.
-        else if (i == 0 && inp[0] == '-') {
+        else if (i == 0 && str[0] == '-') {
             isNegative = true;
             continue;
-         } else if (i == 0 && inp[0] == '+') continue;
+         } else if (i == 0 && str[0] == '+') continue;
          else return false;
       }
       if (hasDot) numValue /= Math.Pow (10, dotPosition);
